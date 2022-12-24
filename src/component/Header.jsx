6bad9814ffe9c0d400ca1551890db74db77e131d
Dadash Hussein <huseynzade.dadas@gmail.com>
2022-12-25 00:09:41 +0400

    color change

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav className="text-center py-3 font-semibold bg-orange-400  my-8 rounded-lg">
+    <nav className="text-center py-3 font-semibold bg-green-400  my-8 rounded-lg">
       <span class=" text-2xl text-white py-6">
         To do App
       </span>
