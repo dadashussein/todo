e301afb2946a10d0ffc0ce0a465a9fca1362310c
Dadash Hussein <huseynzade.dadas@gmail.com>
2022-12-24 23:25:52 +0400

    add to tailwind

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
-import './App.css';
-import React,{ useState , useEffect} from 'react';
+import { useState , useEffect } from "react";
+import "./App.css";
 //components
-import Header from './components/Header/Header';
-import Form from './components/Form/Form';
-import TodosList from './components/Todoslist/TodosList';
+import Form from "./component/Form";
+import Header from "./component/Header";
+import TodoList from "./component/TodoList";
 
-
-const App=()=> {
-  const initalState = JSON.parse(localStorage.getItem('todos')) || [];
-  const [input, setInput] = useState('');
-  const [todos, setTodos] = useState(initalState);
+const App = () => {
+  const initialState=JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
   const [editTodo, setEditTodo] = useState(null);
 
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
   return (
-   <div className='container'>
-    <Header />
-    <Form 
-    input={input}
-    todos={todos}
-    setTodos={setTodos}
-    setInput={setInput}
-    editTodo={editTodo}
-    setEditTodo={setEditTodo}
-    />
-    <TodosList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo}/>
-   </div>
+    <div className="max-w-sm ml-4 first">
+      <Header />
+      <Form
+        input={input}
+        todos={todos}
+        setTodos={setTodos}
+        setInput={setInput}
+        editTodo={editTodo}
+        setEditTodo={setEditTodo}
+      />
+      <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+    </div>
   );
-}
+};
 
 export default App;
